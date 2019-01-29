f4e3f604ff3d0154368c49ebbf99c0547ac9f14f
Geordie Connell <geordie.connell@gmail.com>
2019-01-29 11:43:57 -0500

    formatting html and html templates

---
+++ scripts/bookmarks.js
@@ -0,0 +1,23 @@
+'use strict';
+
+//detailed bookmark template
+<li class="selected bookmark-li">
+  <h3 class="bookmark-title">Bookmark Title</h3>
+  <p class="descrition">
+    boomark descrition
+  </p>
+  <button class="bookmark-link">
+    <a href="#">Visit Site</a>
+  </button>
+  <div class="rating-view">
+    //put rating into this div
+  </div>
+</li>;
+
+//condensed bookmark template
+<li class="bookmark-li">
+    <h3 class="bookmark-title">Bookmark Title</h3>
+    <div class="rating-view">
+    //put rating into this div
+    </div>
+</li>;
